4f3459d993d3fc88b797e742f42d2d074323f0df
Pete Hanner <petehanner@gmail.com>
2019-08-08 12:48:17 -0400

    Song methods

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,51 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    metadata = filename.gsub('.mp3', '').split(' - ')
+    artist_name = metadata[0]
+    name = metadata[1]
+    song = self.new_by_name(name)
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
